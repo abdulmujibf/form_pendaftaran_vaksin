bf4ed67160f284d4950fe5132e9f9b8a1fe4da8b
abdulmujibf <abdulmujib628@gmail.com>
2022-01-26 17:18:31 +0700

    Update

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+var bodyParser = require('body-parser');
+const PORT = 3000
+
+app.set('view engine', 'ejs');
+
+//set upp public directory to serve static files
+app.use(express.static('public'));
+
+//Initiate bodyParser to parse request body
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+app.use('/', (req, res) => {
+  res.render("index")
+})
+
+app.listen(PORT, () => {
+  console.log('this app is running')
+}) 
